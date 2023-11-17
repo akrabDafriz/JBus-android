baf83d12a7cbd83d889b9cf23c6f5de661f9cf02
akrabDafriz <muhammad.daffa27@ui.ac.id>
2023-11-17 11:55:13 +0700

    CS08 - Belum selesai

--- app/src/main/java/com/MuhammadDaffaRizkyandriJBusAF/jbus_android/LoginActivity.java
+++ app/src/main/java/com/MuhammadDaffaRizkyandriJBusAF/jbus_android/LoginActivity.java
@@ -2,13 +2,38 @@ package com.MuhammadDaffaRizkyandriJBusAF.jbus_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    private TextView registerNow =null;
+    private Button loginButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        registerNow = findViewById(R.id.register);
+        loginButton = findViewById(R.id.loginButton);
+
+        registerNow.setOnClickListener(v->{
+            moveActivity(this, RegisterActivity.class);
+        });
+        loginButton.setOnClickListener(v->{
+            moveActivity(this, MainActivity.class);
+        });
+    }
+
+    private void moveActivity(Context ctx, Class<?> cls){
+        Intent intent = new Intent(ctx, cls);
+        startActivity(intent);
+    }
+    private void Toast(Context ctx, String message){
+        Toast.makeText(ctx, message, Toast.LENGTH_SHORT).show();
     }
 }
